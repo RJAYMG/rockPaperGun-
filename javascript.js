9f3aa1a53b3142036a6e7a7b95e7637a80090c02
RJAYMG <100469262+RJAYMG@users.noreply.github.com>
2026-01-17 12:52:51 +0000

    added the array of choices

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
-function getComputerChoice() {
-    randomChoice = Math.floor(Math.random() * 3 + 1)
+const choice = ["Rock", "Paper", "Scissor", "Gun"]
+
+function getComputerChoice(choice) {
+    randomChoice = Math.floor(Math.random() * choice.length)
 }
