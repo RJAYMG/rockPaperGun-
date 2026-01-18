3dd3eda9c34ef8deb88d4524ff286ae2dadd6627
RJAYMG <100469262+RJAYMG@users.noreply.github.com>
2026-01-18 00:45:07 +0000

    fixed some bugs and added the prerequisite rock paper scissor

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
-function getComputerChoice(choice) {
-    choice = ["Rock", "Paper", "Scissor", "Gun"]
-    randomChoice = Math.floor(Math.random() * choice.length)
-    return choice[randomChoice]
+let gameOver = false
+
+let choice = ["rock", "paper", "scissor"]
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * choice.length);
+    return choice[randomChoice];
 }
 
-function getHumanChoice(choice) {
-    choice = ["Rock", "Paper", "Scissor", "Gun"]
-    prompt("Rock, paper, scisor or ...?").toLowerCase()
-    if (choice.include(pick)) {
-        return pick
+function getHumanChoice() {
+    pick = prompt("Rock, paper, scisor...?").toLowerCase();
+    if (choice.includes(pick)) {
+        return pick;
+    } else {
+        return "Invalid value"
+    }
+}
+
+function win() {
+    return "You win!"
+}
+
+function lose() {
+    return "You lose!"
+}
+
+while (!gameOver === true) {
+    if (getComputerChoice==="rock" && getHumanChoice==="scissor") {
+        console.log(lose())
+    } else if (getComputerChoice==="paper" && getHumanChoice==="rock") {
+        console.log(lose())
     }
 }
 
