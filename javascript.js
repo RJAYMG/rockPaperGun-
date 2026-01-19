0a0ee8c926c3fef69103e5f0cd497fd220a59675
RJAYMG <100469262+RJAYMG@users.noreply.github.com>
2026-01-19 10:54:48 +0000

    i just got back from school, wow yeah i don't remember the changers anymore lol. but yeah i discovered syntax, logical errors. i hope i can do better on the next project

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-let gameOver = false
-
 let choice = ["rock", "paper", "scissor"]
 
 function getComputerChoice() {
@@ -8,43 +6,65 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    pick = prompt("Rock, paper, scisor...?").toLowerCase();
+    let pick = prompt("Rock, paper, scisor...?").toLowerCase().trim();
     if (choice.includes(pick)) {
         return pick;
+    }
+}
+
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice()
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice==="rock" && computerChoice==="scissor") {
+        humanScore += 1
+        return "You win this round, Rock beats Scissor"
+    } else if (humanChoice==="paper" && computerChoice==="rock"){
+        humanScore += 1
+        return "You win this round, Paper beats Rock"
+    } else if (humanChoice==="scissor" && computerChoice==="paper") {
+        humanScore += 1
+        return "You win this round, Scissor beats Paper"
+    } else if (computerChoice==="rock" && humanChoice==="scissor") {
+        computerScore += 1
+        return "You lose this round, Rock beats Scissor"
+    } else if (computerChoice==="paper" && humanChoice==="rock"){
+        computerScore += 1
+        return "You lose this round, Paper beats Rock"
+    } else if (computerChoice==="scissor" && humanChoice==="paper") {
+        computerScore += 1
+        return "You lose this round, Scissor beats Paper"
+    } else if (humanChoice==="rock" && computerChoice==="rock") {
+        return "This round is Tie!"
+    } else if (humanChoice==="paper" && computerChoice==="paper"){
+        return "This round is Tie!"
+    } else if (humanChoice==="scissor" && computerChoice==="scissor") {
+        return "This round is Tie!"
     } else {
-        return "Invalid value"
+        return "Invalid Value!"
     }
 }
 
-function win() {
-    return "You win!"
-}
+function playGame() {
+    for (i = 1; i<6; i++) {
+        console.log(`Round ${i}`)
+        let roundResult = playRound(getHumanChoice(), getComputerChoice())
+        console.log(roundResult)
+        console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`)
+        if (i == 5) {
+            console.log("Oops, end of the game")
+        }
+    }
 
-function lose() {
-    return "You lose!"
-}
-
-console.log(getComputerChoice())
-console.log(getHumanChoice())
-
-while (!gameOver === true) {
-    if (getComputerChoice==="rock" && getHumanChoice==="scissor") {
-        console.log(lose())
-        gameOver == true
-    } else if (getComputerChoice==="paper" && getHumanChoice==="rock") {
-        console.log(lose())
-        gameOver == true
-    } else if (getComputerChoice==="scissor" && getHumanChoice==="paper") {
-        console.log(lose())
-        gameOver == true
-    } else if (getHumanChoice==="rock" && getComputerChoice==="scissor") {
-        console.log(lose())
-        gameOver == true
-    } else if (getHumanChoice==="paper" && getComputerChoice==="rock") {
-        console.log(lose())
-        gameOver == true
-    } else if (getHumanChoice==="scissor" && getHumanChoice==="paper") {
-        console.log(lose())
-        gameOver == true
+    if (humanScore > computerScore) {
+        console.log("You win the game!")
+    } else if (computerScore > humanScore) {
+        console.log("You lose the game!")
+    } else if (computerScore == humanScore) {
+        console.log("Tie?")
     }
 }
+
+playGame()
